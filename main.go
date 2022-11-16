2bc40168e07f3218619ba9f2e2753e7dfff92914
usmansadik <usmansadik96@gmail.com>
2022-11-16 18:56:53 +0100

    chore: setup project structure

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "server online",
+		})
+	})
+
+	router.Run()
+}
