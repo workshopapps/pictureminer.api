5e0cb36525aaaf8c6da28e2edb324a2fee411bb4
usmansadik <usmansadik96@gmail.com>
2022-11-17 14:22:03 +0100

    feat: implement endpoint to generate caption from image upload

--- microservice/app/routes/mine_image.py
+++ microservice/app/routes/mine_image.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
-
-from app.schemas import ImageUpload
+from fastapi import APIRouter, UploadFile
 
 router = APIRouter(prefix='/api/microservice', tags=['/api/microservice'])
 
 
 @router.get('/content')
-async def get_image_content(image: ImageUpload):
-    return image
+async def get_image_content(image: UploadFile):
+    image_filename, image_body = image.filename, image.file
+
+    # call function to caption/generate text from model sub dir
+    # possibly async
+    # captioned_text = generate_caption(image_filename, image_body)
+    captioned_text = '<dummy captioned text>'
+
+    return {'text_description': captioned_text}
