055173b9d1046914b7878106435ffa6a68ae1c50
Ebenezer Olayinka Adeniyi <61415203+Adeben33@users.noreply.github.com>
2022-11-18 22:40:22 +0100

    Update user.go

--- pkg/handler/user/user.go
+++ pkg/handler/user/user.go
@@ -1,15 +1,47 @@
 package user
 
 import (
-	"net/http"
-
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/workshopapps/pictureminer.api/internal/model"
+	"github.com/workshopapps/pictureminer.api/pkg/repository/storage/mongodb"
 	"github.com/workshopapps/pictureminer.api/utility"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
 )
 
-func (base *Controller) CreateUser(c *gin.Context) {
+//var (
+//	ctx         context.Context
+//	mongoClient *mongo.Client
+//	db          *mongo.Database
+//)
 
-	rd := utility.BuildSuccessResponse(http.StatusCreated, "user created successfully", gin.H{"user": "user object"})
+func (base *Controller) Signup(c *gin.Context) {
+	//Database connection
+	mongoClient := mongodb.Connection()
+	imageDB := mongoClient.Database("ImageCollection")
+	userCollection := imageDB.Collection("user")
+
+	//Binding the userdetails to userStruct
+	var User model.UserStruct
+	err := c.Bind(&User)
+	if err != nil {
+		log.Fatal("Unable to bind user signup details")
+	}
+	//	Hashing the password
+	harsh, err := bcrypt.GenerateFromPassword([]byte(User.Password), 10)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to harsh password",
+		})
+		return
+	}
+	User.Password = string(harsh)
+	User.Token = utility.CreateToken()
+	userCollection.InsertOne(context.Background(), User)
+
+	rd := utility.BuildSuccessResponse(http.StatusCreated, "user created successfully", gin.H{"user": User})
 	c.JSON(http.StatusOK, rd)
 
 }
