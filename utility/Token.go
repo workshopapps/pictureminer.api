64fc7bf4fb50743581d7b079366e0a7765b180d1
Ebenezer Olayinka Adeniyi <61415203+Adeben33@users.noreply.github.com>
2022-11-19 10:13:00 +0100

    Create Token.go

---
+++ utility/Token.go
@@ -0,0 +1,24 @@
+package utility
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/workshopapps/pictureminer.api/internal/model"
+	"log"
+	"time"
+)
+
+func CreateToken(user *model.UserStruct) *string {
+	mysecretekey := "Secretkey"
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"nbf":   time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(mysecretekey))
+	if err != nil {
+		log.Panic("Error is getting jwt key")
+	}
+
+	return &tokenString
+
+}
