5db909cb4bfe366079c7a50c528ec724e2a69ca9
Emmrys-Jay <jonathanemma121@gmail.com>
2022-11-20 07:00:36 +0100

    feat(BE-24): complete mine service upload endpoint

--- go.mod
+++ go.mod
@@ -5,12 +5,19 @@ go 1.17
 require (
 	github.com/aws/aws-sdk-go v1.44.140
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-playground/locales v0.14.0
+	github.com/go-playground/universal-translator v0.18.0
+	github.com/go-playground/validator/v10 v10.10.0
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/stretchr/testify v1.8.1
 	go.mongodb.org/mongo-driver v1.11.0
 )
 
 require (
-	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07 // indirect
 )
 
@@ -21,11 +28,7 @@ require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gin-contrib/gzip v0.0.6
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0
-	github.com/go-playground/universal-translator v0.18.0
-	github.com/go-playground/validator/v10 v10.11.1
-	github.com/go-redis/redis/v8 v8.11.5
-	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jeanphorn/log4go v0.0.0-20190526082429-7dbb8deb9468
@@ -33,13 +36,13 @@ require (
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/afero v1.9.2 // indirect
@@ -53,7 +56,7 @@ require (
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
--- pkg/repository/storage/mongodb/mongo.go
+++ pkg/repository/storage/mongodb/mongo.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/workshopapps/pictureminer.api/internal/constants"
+	"github.com/workshopapps/pictureminer.api/internal/model"
+	"go.mongodb.org/mongo-driver/bson"
+
 	"github.com/workshopapps/pictureminer.api/internal/config"
 	"github.com/workshopapps/pictureminer.api/utility"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,18 +18,19 @@ import (
 
 var (
 	ctx         context.Context
-	mongoclient *mongo.Client
+	mongoClient *mongo.Client
 )
 
 func Connection() (db *mongo.Client) {
-	return mongoclient
+	return mongoClient
 }
 
 func ConnectToDB() *mongo.Client {
+	var err error
 	logger := utility.NewLogger()
 	uri := config.GetConfig().Mongodb.Url
 	mongo_connection := options.Client().ApplyURI(uri)
-	mongoClient, err := mongo.Connect(ctx, mongo_connection)
+	mongoClient, err = mongo.Connect(ctx, mongo_connection)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,7 +45,6 @@ func ConnectToDB() *mongo.Client {
 	fmt.Println("MONGO CONNECTION ESTABLISHED")
 	logger.Info("MONGO CONNECTION ESTABLISHED")
 
-	mongoclient = mongoClient
 	return mongoClient
 }
 
@@ -48,4 +52,68 @@ func ConnectToDB() *mongo.Client {
 func GetCollection(client *mongo.Client, databaseName, collectionName string) *mongo.Collection {
 	collection := client.Database(databaseName).Collection(collectionName)
 	return collection
+}
+
+//creating a user in the data base
+
+func CreateUser(context context.Context, User model.User) {
+	imageDB := mongoClient.Database(constants.UserDatabase)
+	userCollection := imageDB.Collection(constants.UserDatabase)
+	userCollection.InsertOne(context, User)
+}
+
+func MongoPost(collection string, data interface{}) (*mongo.InsertOneResult, error) {
+	c := getCollection(collection)
+
+	result, err := c.InsertOne(ctx, data)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func MongoGet(collection string, filter map[string]interface{}) ([]interface{}, error) {
+	c := getCollection(collection)
+
+	f := make(bson.M, 0)
+
+	if len(filter) == 1 {
+		for k, v := range filter {
+			f = bson.M{k: v}
+		}
+	} else if len(filter) > 1 {
+		tf := make([]bson.M, 0, len(filter))
+		for k, v := range filter {
+			tf = append(tf, bson.M{k: v})
+		}
+
+		f = bson.M{"$and": tf}
+	}
+
+	cursor, err := c.Find(ctx, f)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]interface{}, 0)
+
+	for cursor.Next(ctx) {
+		var result interface{}
+		err := cursor.Decode(&result)
+		if err != nil {
+			return nil, err
+		}
+		results = append(results, result)
+	}
+
+	return results, nil
+}
+
+func getCollection(collection string) *mongo.Collection {
+	databaseName := config.GetConfig().Mongodb.Database
+	database := mongoClient.Database(databaseName)
+	c := database.Collection(collection)
+
+	return c
 }
