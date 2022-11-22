edac258dd419c6a3fea808f927199a354ca8132b
Abubakar Usman <usmansadik96@gmail.com>
2022-11-22 19:58:40 +0100

    Revert "Delete User By admin"

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,13 +5,12 @@ import (
 )
 
 type User struct {
-	ID        primitive.ObjectID 	`bson:"_id, omitempty"`
-	Username  	string             	`bson:"username" json:"username" validate:"required"`
-	FirstName 	string             	`bson:"first_name" json:"first_name"`
-	LastName  	string             	`bson:"last_name" json:"last_name"`
-	Email     	string             	`bson:"email" json:"email" validate:"required,email"`
-	Password  	string             	`bson:"password" json:"password" validate:"required"`
-	User_Id		string				`json:"user_id"`
+	ID        primitive.ObjectID `bson:"_id, omitempty"`
+	Username  string             `bson:"username" json:"username" validate:"required"`
+	FirstName string             `bson:"first_name" json:"first_name"`
+	LastName  string             `bson:"last_name" json:"last_name"`
+	Email     string             `bson:"email" json:"email" validate:"required,email"`
+	Password  string             `bson:"password" json:"password" validate:"required"`
 }
 
 type UserResponse struct {
--- service/admin/admin.go
+++ service/admin/admin.go
@@ -2,17 +2,12 @@ package admin
 
 import (
 	"context"
-	"fmt"
-	"net/http"
-	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/workshopapps/pictureminer.api/internal/config"
 	"github.com/workshopapps/pictureminer.api/internal/constants"
 	"github.com/workshopapps/pictureminer.api/internal/model"
 	"github.com/workshopapps/pictureminer.api/pkg/repository/storage/mongodb"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetUsers() ([]model.User, error) {
@@ -27,24 +22,4 @@ func GetUsers() ([]model.User, error) {
 	cursor.All(ctx, &users)
 
 	return users, nil
-}
-
-// DeleteUser Function
-func DeleteUser() gin.HandlerFunc{
-	return func(c *gin.Context) {
-		userId := c.Param("_id")
-
-		var userCollection *mongo.Collection = mongodb.OpenCollection(mongodb.Client, "user")
-		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
-		var user *model.User
-
-		res, err := userCollection.DeleteOne(ctx, bson.D{{userId, &user.User_Id}})
-		defer cancel()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		fmt.Printf("deleted %v documents\n", res.DeletedCount)
-		c.JSON(http.StatusOK, &user)
-	}
 }
