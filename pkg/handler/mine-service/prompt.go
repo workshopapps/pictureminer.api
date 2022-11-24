a03cd63e2348a212954f4d62aa19aa97b89423c4
Emmrys-Jay <jonathanemma121@gmail.com>
2022-11-24 22:19:47 +0100

    fix(BE-39):Move token functions for prompt to utility

--- pkg/handler/mine-service/prompt.go
+++ pkg/handler/mine-service/prompt.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/workshopapps/pictureminer.api/internal/config"
 	"github.com/workshopapps/pictureminer.api/internal/model"
 	mineservice "github.com/workshopapps/pictureminer.api/service/mine-service"
 	"github.com/workshopapps/pictureminer.api/utility"
@@ -11,8 +12,9 @@ import (
 
 func (base *Controller) PromptMineImageUpload(c *gin.Context) {
 
-	token := extractToken(c)
-	_, err := getKey("id", token)
+	secretKey := config.GetConfig().Server.Secret
+	token := utility.ExtractToken(c)
+	_, err := utility.GetKey("id", token, secretKey)
 	if err != nil {
 		rd := utility.BuildErrorResponse(http.StatusUnauthorized, "failed", "could not verify token", nil, gin.H{"error": err.Error()})
 		c.JSON(http.StatusUnauthorized, rd)
