e2904bf5634490bb48f89ba7fcf926de0e4dddb3
usmansadik <usmansadik96@gmail.com>
2022-12-01 16:15:12 +0100

    fix: delete files causing issue

--- pkg/router/mine-service.go
+++ pkg/router/mine-service.go
@@ -15,8 +15,6 @@ func MineServiceUpload(r *gin.Engine, validate *validator.Validate, ApiVersion s
 	authUrl := r.Group(fmt.Sprintf("/api/%v", ApiVersion))
 	{
 		authUrl.POST("/mine-service/upload", mineservice.MineImageUpload)
-		authUrl.POST("/mine-service/upload-prompt", mineservice.PromptMineImageUpload)
-		authUrl.POST("/mine-service/url-prompt", mineservice.PromptMineImageUrl)
 		authUrl.POST("/mine-service/url", mineservice.MineImageUrl)
 		authUrl.GET("/mine-service/get-all", mineservice.GetMinedImages)
 	}
