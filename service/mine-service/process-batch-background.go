480b22b3b167593611b99aac6d98ab62eab7fbe0
usmansadik <usmansadik96@gmail.com>
2022-12-02 12:25:03 +0100

    feat: implement functionality to retreive labels for all urls using the api

--- service/mine-service/process-batch-background.go
+++ service/mine-service/process-batch-background.go
@@ -1,7 +1,50 @@
 package mineservice
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/workshopapps/pictureminer.api/internal/config"
+)
+
+type Result struct {
+	Name  string
+	Score float64
+}
+
+type Label struct {
+	URL     string
+	Results []Result
+}
+
+type (
+	APIResponse struct {
+		Result APIResponseResult `json:"result"`
+	}
+
+	APIResponseResult struct {
+		Tags []Tag `json:"tags"`
+	}
+
+	Tag struct {
+		Confidence float64           `json:"confidence"`
+		Tag        map[string]string `json:"tag"`
+	}
+)
+
+const (
+	Threshold = "40"
+	Limit     = "5"
+)
+
 func processBatch(email, batchID, name, desc string, tags, urls []string) {
 	// labels for each url
+	labels := fetchLabelsForURLS(urls)
+
+	for _, label := range labels {
+		fmt.Println(label)
+	}
 
 	// classify url to tag
 
@@ -10,8 +53,50 @@ func processBatch(email, batchID, name, desc string, tags, urls []string) {
 	// on complete, notify user through email
 }
 
-func fetchLabelsForURLS(urls []string) {
+func fetchLabelsForURLS(urls []string) []Label {
+	var labels []Label
+	ImaggaURL := config.GetConfig().ImaggaAPI.URL
+	AuthToken := config.GetConfig().ImaggaAPI.Auth
+	httpClient := &http.Client{}
+	for _, url := range urls {
+		label := getLabel(httpClient, ImaggaURL, url, AuthToken, Limit, Threshold)
+		labels = append(labels, label)
+	}
+	return labels
+}
 
+func getLabel(client *http.Client, imaggaURL, url, authToken, limit, threshold string) Label {
+	req, _ := http.NewRequest("GET", imaggaURL, nil)
+	req.Header.Set("Authorization", authToken)
+	q := req.URL.Query()
+	q.Add("limit", limit)
+	q.Add("threshold", threshold)
+	q.Add("image_url", url)
+	req.URL.RawQuery = q.Encode()
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	var apiRes APIResponse
+	err = json.NewDecoder(res.Body).Decode(&apiRes)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var results []Result
+	for _, tag := range apiRes.Result.Tags {
+		results = append(results, Result{Name: tag.Tag["en"], Score: tag.Confidence})
+	}
+
+	var label = Label{
+		URL:     url,
+		Results: results,
+	}
+
+	return label
 }
 
 func classifyLabels() {
