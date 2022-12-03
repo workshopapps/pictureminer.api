e34f30b1aa45839e67eb3cab8a01e73294490bee
noornee <noornee.011@gmail.com>
2022-12-03 15:12:34 +0100

    Revert "feat(BE-60): implemented a handler to return the list of parsed urls"
    This reverts commit 13cb4f012731c16177e6b72eed685d014681a6cc.

--- pkg/router/mine-service.go
+++ pkg/router/mine-service.go
@@ -17,7 +17,6 @@ func MineServiceUpload(r *gin.Engine, validate *validator.Validate, ApiVersion s
 		authUrl.POST("/mine-service/upload", mineservice.MineImageUpload)
 		authUrl.POST("/mine-service/url", mineservice.MineImageUrl)
 		authUrl.GET("/mine-service/get-all", mineservice.GetMinedImages)
-		authUrl.POST("/mine-service/upload-csv", mineservice.ParseUrlFromCsv)
 	}
 	return r
 }
