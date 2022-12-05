d741244b8a725409942ee3e7acf805094e6f33a8
lordscoba <lordscoba2tm@gmail.com>
2022-12-05 16:33:04 +0100

    Feat/BE-69(Improvement on filter tags)

--- service/mine-service/batch-service.go
+++ service/mine-service/batch-service.go
@@ -42,25 +42,76 @@ response = filterTags(length,image_collection,tag,tags)
 
 }
 
-func filterTags(length int, image_collection []model.BatchImage ,tag []string,tags []string) []TagOne{
 
-  var str []TagOne
+func FilterTags(length int,image_collection []models.Image,tag []string,tags []string) []models.TagOne{
+
+  var str []models.TagOne
 
   for k := 0; k < length ; k++ {
-			var tagone TagOne
+  var tagone models.TagOne
+  tagone.Tag = tags[k]
   for i , test:= range image_collection{
-			tagone.Tag = tags[k]
-  if tag[i] == tags[k]{
-			fmt.Println(tag[i]+tags[k])
-			fmt.Println(test.URL)
-			 var urlone UrlOne
-	     urlone.Url = test.URL
-	     tagone.Data = append(tagone.Data , urlone )
-			fmt.Println(tagone.Data)
-    	}
-  	}
-	str = append(str, tagone)
+    if tag[i] == tags[k]{
+      var urlone models.UrlOne
+      urlone.Url = test.Url
+      tagone.Data = append(tagone.Data , urlone )
+    }
+  }
+  str = append(str, tagone)
   }
 
+
+  DistagsBatch := unique(tags)
+  DisttagImage := unique(tag)
+  diff := missing(DistagsBatch, DisttagImage)
+
+  fmt.Println(diff)
+  var tagone models.TagOne
+  tagone.Tag = "untagged"
+  for p := 0; p < len(diff) ; p++ {
+      for _ , test:= range image_collection{
+          if diff[p] == test.Tag {
+            var urlTwo models.UrlOne
+            urlTwo.Url = test.Url
+            tagone.Data = append(tagone.Data , urlTwo)
+            fmt.Println(test.Url)
+          }
+        }
+      }
+      str = append(str, tagone)
+      fmt.Println(str)
 return str
+}
+
+
+func unique(intSlice []string) []string {
+    keys := make(map[string]bool)
+    list := []string{}
+    for _, entry := range intSlice {
+        if _, value := keys[entry]; !value {
+            keys[entry] = true
+            list = append(list, entry)
+        }
+    }
+    return list
+}
+
+
+// missing compares two slices and returns slice of differences
+func missing(a, b []string) []string {
+    type void struct{}
+	// create map with length of the 'a' slice
+	ma := make(map[string]void, len(a))
+	diffs := []string{}
+	// Convert first slice to map with empty struct (0 bytes)
+	for _, ka := range a {
+		ma[ka] = void{}
+	}
+	// find missing values in a
+	for _, kb := range b {
+		if _, ok := ma[kb]; !ok {
+			diffs = append(diffs, kb)
+		}
+	}
+	return diffs
 }
